bc34cf2135b5af36a7e12723e03acd1347c913ec
Mika Hoppe <mika.hoppe@uni-potsdam.de>
2022-04-10 16:12:07 +0200

    Add horizontal line every 3 personas

--- views/components/Persona.tsx
+++ views/components/Persona.tsx
@@ -10,39 +10,52 @@ export const PersonaView = ({
 	person: Persona;
 }) => {
 	return (
-		<View
-			style={{
-				paddingTop: 20,
-				paddingBottom: 20,
-				paddingLeft: 20,
-				paddingRight: 150,
-				width: "100%",
-				display: "flex",
-				flexDirection: "row",
-				alignItems: "center",
-				backgroundColor: index % 2 === 0 ? "transparent" : "#333333",
-			}}
-		>
-			<Text
+		<>
+			<View
 				style={{
-					marginRight: 20,
-					color: "#F7FAF4",
-					fontFamily: "SourceSansProBold",
-					fontSize: 36,
+					paddingTop: 20,
+					paddingBottom: 20,
+					paddingLeft: 20,
+					paddingRight: 150,
+					width: "100%",
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					backgroundColor:
+						index % 2 === 0 ? "transparent" : "#333333",
 				}}
 			>
-				{index + 1}
-			</Text>
-			<Text
-				style={{
-					color: "#F7FAF4",
-					fontFamily: "SourceSansProRegular",
-					fontSize: 20,
-				}}
-			>
-				{person.gender} {person.name}, {person.age} Jahre alt,{" "}
-				{person.profession}, {person.illness}, {person.characteristic}
-			</Text>
-		</View>
+				<Text
+					style={{
+						marginRight: 20,
+						color: "#F7FAF4",
+						fontFamily: "SourceSansProBold",
+						fontSize: 36,
+					}}
+				>
+					{index + 1}
+				</Text>
+				<Text
+					style={{
+						color: "#F7FAF4",
+						fontFamily: "SourceSansProRegular",
+						fontSize: 20,
+					}}
+				>
+					{person.gender} {person.name}, {person.age} Jahre alt,{" "}
+					{person.profession}, {person.illness},{" "}
+					{person.characteristic}
+				</Text>
+			</View>
+			{index % 3 === 2 && (
+				<View
+					style={{
+						width: "100%",
+						height: 3,
+						backgroundColor: "#FFFFFF",
+					}}
+				></View>
+			)}
+		</>
 	);
 };
